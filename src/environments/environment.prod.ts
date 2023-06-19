2973f0addfcb949e5d943f04143b765459424fc8
Taimoors7 <taimoorsajjad980@gmail.com>
2023-06-19 13:49:11 +0500

    Firebase firestore attached

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  firebase: {
+    apiKey: 'AIzaSyCNLr6NvpLlOENNs_xfsPKJitf6f5_vm8E',
+    authDomain: 'inventorylocator-241c3.firebaseapp.com',
+    projectId: 'inventorylocator-241c3',
+    storageBucket: 'inventorylocator-241c3.appspot.com',
+    messagingSenderId: '211413166747',
+    appId: '1:211413166747:web:ea60206882dc950cf7343e',
+  },
+  production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    apiKey: 'AIzaSyCNLr6NvpLlOENNs_xfsPKJitf6f5_vm8E',
+    authDomain: 'inventorylocator-241c3.firebaseapp.com',
+    projectId: 'inventorylocator-241c3',
+    storageBucket: 'inventorylocator-241c3.appspot.com',
+    messagingSenderId: '211413166747',
+    appId: '1:211413166747:web:ea60206882dc950cf7343e',
+  },
+  production: false,
 };
 
 /*
