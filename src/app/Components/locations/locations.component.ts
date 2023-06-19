42c953a24e20b5cfcf2aa13a162cb505dd1fc9d9
Taimoors7 <taimoorsajjad980@gmail.com>
2023-06-20 00:44:22 +0500

    Form design to add location

--- src/app/Components/locations/locations.component.ts
+++ src/app/Components/locations/locations.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore, collection } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-locations',
   templateUrl: './locations.component.html',
-  styleUrls: ['./locations.component.css']
+  styleUrls: ['./locations.component.css'],
 })
 export class LocationsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  collectionRef;
+  constructor(private firestore: Firestore, private router: Router) {
+    this.collectionRef = collection(this.firestore, 'locations');
   }
 
+  ngOnInit(): void {}
+  addLocation() {
+    this.router.navigate(['/addlocation']);
+  }
 }
