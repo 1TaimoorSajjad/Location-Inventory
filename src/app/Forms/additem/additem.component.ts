c264e35ed22ab432ccc6d1d5c5705dc679177634
Taimoors7 <taimoorsajjad980@gmail.com>
2023-06-20 17:16:57 +0500

    Fixing the location items

--- src/app/Forms/additem/additem.component.ts
+++ src/app/Forms/additem/additem.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Firestore, addDoc, collection } from '@angular/fire/firestore';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+} from '@angular/fire/firestore';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -11,6 +18,8 @@ import { Router } from '@angular/router';
 export class AdditemComponent implements OnInit {
   itemForm!: FormGroup;
   collectionRef;
+  locations: any[] = [];
+
   constructor(
     private firestore: Firestore,
     private router: Router,
@@ -22,18 +31,48 @@ export class AdditemComponent implements OnInit {
   ngOnInit(): void {
     this.itemForm = this.fb.group({
       itemName: [''],
+      location: [''],
     });
+
+    this.fetchLocations();
+  }
+
+  fetchLocations() {
+    const locationsRef = collection(this.firestore, 'locations');
+    getDocs(locationsRef)
+      .then((querySnapshot) => {
+        this.locations = [];
+        querySnapshot.forEach((doc) => {
+          this.locations.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+      })
+      .catch((error: any) => {
+        console.log('Error fetching locations', error);
+      });
   }
 
   onSubmit() {
     const formData = this.itemForm.value;
+    const selectedLocationId = formData.location;
+
+    const selectedLocation = this.locations.find(
+      (location) => location.id === selectedLocationId
+    );
+    if (!selectedLocation) {
+      console.log('Invalid location selected');
+      return;
+    }
+
     addDoc(this.collectionRef, formData)
       .then(() => {
         console.log('Item added in the table');
         this.router.navigate(['/items']);
       })
       .catch((error) => {
-        console.log('Error sending the data to database');
+        console.log('Error sending the data to the database', error);
       });
   }
 }
--- src/app/Forms/addvariant/addvariant.component.ts
+++ src/app/Forms/addvariant/addvariant.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Firestore, addDoc, collection } from '@angular/fire/firestore';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  query,
+  getDocs,
+} from '@angular/fire/firestore';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -11,6 +17,8 @@ import { Router } from '@angular/router';
 export class AddvariantComponent implements OnInit {
   variantForm!: FormGroup;
   collectionRef;
+  items: any[] = [];
+
   constructor(
     private router: Router,
     private firestore: Firestore,
@@ -23,17 +31,37 @@ export class AddvariantComponent implements OnInit {
     this.variantForm = this.fb.group({
       variantName: [''],
       quantity: [''],
+      item: [''],
     });
+
+    this.fetchItems();
   }
+
+  fetchItems() {
+    const itemsRef = collection(this.firestore, 'items');
+    const q = query(itemsRef);
+
+    getDocs(q)
+      .then((querySnapshot) => {
+        this.items = [];
+        querySnapshot.forEach((doc) => {
+          this.items.push({ id: doc.id, ...doc.data() });
+        });
+      })
+      .catch((error) => {
+        console.log('Error fetching items', error);
+      });
+  }
+
   onSubmit() {
     const formData = this.variantForm.value;
     addDoc(this.collectionRef, formData)
       .then(() => {
-        console.log('Variant added in the database');
+        console.log('Variant added to the database');
         this.router.navigate(['/variants']);
       })
-      .catch((err) => {
-        console.log('error sending data to the database');
+      .catch((error) => {
+        console.log('Error sending data to the database', error);
       });
   }
 }
