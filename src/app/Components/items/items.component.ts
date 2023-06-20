e436bd984e9f408a79bf51624e6caebc0aa661f0
Taimoors7 <taimoorsajjad980@gmail.com>
2023-06-20 13:17:56 +0500

    Items table populated with data from the database

--- src/app/Components/items/items.component.ts
+++ src/app/Components/items/items.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore, collection, query, getDocs } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
-  styleUrls: ['./items.component.css']
+  styleUrls: ['./items.component.css'],
 })
 export class ItemsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  items: any[] = [];
+  collectionRef;
+  constructor(private router: Router, private firestore: Firestore) {
+    this.collectionRef = collection(this.firestore, 'items');
   }
 
+  ngOnInit(): void {
+    this.fetchItems();
+  }
+
+  fetchItems() {
+    const q = query(this.collectionRef);
+    getDocs(q)
+      .then((querySnapshot) => {
+        this.items = [];
+        querySnapshot.forEach((doc) => {
+          this.items.push(doc.data());
+        });
+      })
+      .catch((error: any) => {
+        console.log('Error fetching locations', error);
+      });
+  }
+
+  addItem() {
+    this.router.navigate(['/additem']);
+  }
 }
--- src/app/Forms/additem/additem.component.ts
+++ src/app/Forms/additem/additem.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore, addDoc, collection } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-additem',
   templateUrl: './additem.component.html',
-  styleUrls: ['./additem.component.css']
+  styleUrls: ['./additem.component.css'],
 })
 export class AdditemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  itemForm!: FormGroup;
+  collectionRef;
+  constructor(
+    private firestore: Firestore,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.collectionRef = collection(this.firestore, 'items');
   }
 
+  ngOnInit(): void {
+    this.itemForm = this.fb.group({
+      itemName: [''],
+    });
+  }
+
+  onSubmit() {
+    const formData = this.itemForm.value;
+    addDoc(this.collectionRef, formData)
+      .then(() => {
+        console.log('Item added in the table');
+        this.router.navigate(['/items']);
+      })
+      .catch((error) => {
+        console.log('Error sending the data to database');
+      });
+  }
 }
