fcd6a284add2c837cfa8fb7c21311789c77a2e26
Taimoors7 <taimoorsajjad980@gmail.com>
2023-06-20 14:48:15 +0500

    Added signout in navbar

--- src/app/Components/Common/sidenav/sidenav.component.ts
+++ src/app/Components/Common/sidenav/sidenav.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  browserSessionPersistence,
+  getAuth,
+  onAuthStateChanged,
+  setPersistence,
+  signOut,
+} from 'firebase/auth';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.css'],
 })
 export class SidenavComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  signOut() {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        console.log('Sign out successful');
+        this.router.navigate(['/login']);
+      })
+      .catch((error) => {
+        console.log('Sign out error:', error);
+      });
   }
-
 }
