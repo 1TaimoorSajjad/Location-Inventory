bc886fc89b6d0d7ebad037421a4ab4b8c30800c1
Taimoors7 <taimoorsajjad980@gmail.com>
2023-06-22 19:05:30 +0500

    fixing the variant id issue

--- src/app/Components/locations/locations.component.ts
+++ src/app/Components/locations/locations.component.ts
@@ -24,45 +24,32 @@ export class LocationsComponent implements OnInit {
   }
 
   async getLocations() {
-    try {
-      const locationsCollection = collection(this.firestore, 'locations');
-      const locationsQuery = query(locationsCollection);
-      const locationsSnapshot = await getDocs(locationsQuery);
+    const locationsCollection = collection(this.firestore, 'locations');
+    const locationsQuery = query(locationsCollection);
+    const locationsSnapshot = await getDocs(locationsQuery);
 
-      const locationsData = [];
-      for (const docSnap of locationsSnapshot.docs) {
-        const locationData = docSnap.data();
-        if (locationData.variant) {
-          const variantId = locationData.variant;
-          const variantData = await this.getVariantData(variantId);
-          if (variantData) {
-            locationData.variantName = variantData.variantName;
-          }
+    const locationsData = [];
+    for (const docSnap of locationsSnapshot.docs) {
+      const locationData = docSnap.data();
+      const variantId = locationData.variant;
+      if (typeof variantId === 'string') {
+        const variantData = await this.getVariantData(variantId);
+        if (variantData) {
+          locationData.variantName = variantData.variantName;
         }
-        locationsData.push(locationData);
       }
-
-      this.locations = locationsData;
-    } catch (error) {
-      console.log('Error retrieving locations:', error);
+      locationsData.push(locationData);
     }
+    this.locations = locationsData;
   }
 
   async getVariantData(variantId: string): Promise<any> {
-    try {
-      const variantDocRef = doc(this.firestore, 'variants', variantId);
-      const variantSnapshot = await getDoc(variantDocRef);
-      if (variantSnapshot.exists()) {
-        return variantSnapshot.data();
-      } else {
-        console.log(`Variant document not found for variantId: ${variantId}`);
-        return null;
-      }
-    } catch (error) {
-      console.log(
-        `Error retrieving variant data for variantId: ${variantId}`,
-        error
-      );
+    const variantDocRef = doc(this.firestore, 'variants', variantId);
+    const variantDocSnapshot = await getDoc(variantDocRef);
+    if (variantDocSnapshot.exists()) {
+      return variantDocSnapshot.data();
+    }
+    {
       return null;
     }
   }
